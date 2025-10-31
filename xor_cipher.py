a5aa8a9095fabfb3a3623c15acb9e411600f08c7
Danixdy <dfpazminio@gmail.com>
2025-10-31 12:36:17 -0500

    Add files via upload

--- xor_cipher.py
+++ xor_cipher.py
@@ -1,13 +1,13 @@
 import random
 
 def xor_cipher(texto):
-    clave = ''.join(random.choice("01") for _ in range(len(texto)))
+    clave = ''.join(random.choice('01') for _ in range(len(texto)))
     cifrado = ''.join(chr(ord(texto[i]) ^ int(clave[i])) for i in range(len(texto)))
-    descifrado = ''.join(chr(ord(cifrado[i]) ^ int(clave[i])) for i in range(len(cifrado)))
-    return cifrado, descifrado, clave
+    return cifrado, clave
 
 texto = input("Texto: ")
-cifrado, descifrado, clave = xor_cipher(texto)
+cifrado, clave = xor_cipher(texto)
+descifrado = xor_cipher(cifrado)[0] 
 print(f"Clave: {clave}")
 print(f"Cifrado: {cifrado}")
 print(f"Descifrado: {descifrado}")
